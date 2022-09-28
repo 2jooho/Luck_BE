9c1d9f1a8b1dfa0d528b44d88a102b917a7b18c9
이주호/과장/(주)신세계아이앤씨 <FIC06779@SSG.GLOBAL>
2022-09-28 20:37:31 +0900

    aws test2

--- src/main/java/com/luck/luckApi/controller/LoggingController.java
+++ src/main/java/com/luck/luckApi/controller/LoggingController.java
@@ -1,3 +1,9 @@
+package com.luck.luckApi.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class LoggingController {
 

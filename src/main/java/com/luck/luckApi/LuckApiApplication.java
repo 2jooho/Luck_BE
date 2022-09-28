6265f47b9cd9b2503ef163694bc52b86fd9ccd68
이주호/과장/(주)신세계아이앤씨 <FIC06779@SSG.GLOBAL>
2022-09-28 19:25:55 +0900

    aws test

---
+++ src/main/java/com/luck/luckApi/LuckApiApplication.java
@@ -0,0 +1,13 @@
+package com.luck.luckApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuckApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuckApiApplication.class, args);
+	}
+
+}

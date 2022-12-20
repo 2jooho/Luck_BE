7702933ada679a87f899c1a5c05f525139f0697f
2jooho <tjrwn1003@gmail.com>
2022-12-20 23:26:41 +0900

    12/20 test

--- src/main/java/com/luck/luckApi/LuckApiApplication.java
+++ src/main/java/com/luck/luckApi/LuckApiApplication.java
@@ -2,18 +2,13 @@ package com.luck.luckApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class LuckApiApplication {
 
-	public static final String APPLICATION_LOCATIONS = "spring.config.location="
-			+ "classpath:application.yml,"
-			+ "./app/config/newsum/real-application.yml";
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(LuckApiApplication.class)
-				.properties(APPLICATION_LOCATIONS)
-				.run(args);
+		SpringApplication.run(LuckApiApplication.class, args);
 	}
+
 
 }

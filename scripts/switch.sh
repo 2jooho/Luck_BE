209f4637fe48e8d9e99d4db43461135bafc4f800
2jooho <tjrwn1003@gmail.com>
2022-12-21 00:03:54 +0900

    12/21test

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -1,29 +1,48 @@
-## switch.sh
+### switch.sh
+##
+###!/bin/bash
+##
+### Crawl current connected port of WAS
+##CURRENT_PORT=$(cat /home/ec2-user/service_url.inc  | grep -Po '[0-9]+' | tail -1)
+##TARGET_PORT=0
+##
+##echo "> Nginx currently proxies to ${CURRENT_PORT}."
+##
+### Toggle port number
+##if [ ${CURRENT_PORT} -eq 8081 ]; then
+##    TARGET_PORT=8082
+##elif [ ${CURRENT_PORT} -eq 8082 ]; then
+##    TARGET_PORT=8081
+##else
+##    echo "> No WAS is connected to nginx"
+##    exit 1
+##fi
+##
+### Change proxying port into target port
+##echo "set \$service_url http://127.0.0.1:${TARGET_PORT};" | tee /home/ec2-user/service_url.inc
+##
+##echo "> Now Nginx proxies to ${TARGET_PORT}."
+##
+### Reload nginx
+##sudo service nginx reload
+##
+##echo "> Nginx reloaded."
 #
-##!/bin/bash
 #
-## Crawl current connected port of WAS
-#CURRENT_PORT=$(cat /home/ec2-user/service_url.inc  | grep -Po '[0-9]+' | tail -1)
-#TARGET_PORT=0
 #
-#echo "> Nginx currently proxies to ${CURRENT_PORT}."
+##!/usr/bin/env bash
 #
-## Toggle port number
-#if [ ${CURRENT_PORT} -eq 8081 ]; then
-#    TARGET_PORT=8082
-#elif [ ${CURRENT_PORT} -eq 8082 ]; then
-#    TARGET_PORT=8081
-#else
-#    echo "> No WAS is connected to nginx"
-#    exit 1
-#fi
+#ABSPATH=$(readlink -f $0)
+#ABSDIR=$(dirname $ABSPATH)
+#source ${ABSDIR}/profile.sh
 #
-## Change proxying port into target port
-#echo "set \$service_url http://127.0.0.1:${TARGET_PORT};" | tee /home/ec2-user/service_url.inc
+#function switch_proxy() {
+#    IDLE_PORT=$(find_idle_port)
 #
-#echo "> Now Nginx proxies to ${TARGET_PORT}."
+#    echo "> 전환할 Port: $IDLE_PORT"
+#    echo "> Port 전환"
+#    echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
 #
-## Reload nginx
-#sudo service nginx reload
-#
-#echo "> Nginx reloaded."
+#    echo "> 엔진엑스 Reload"
+#    sudo service nginx reload
+#}

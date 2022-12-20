cd5f15bfd6fac65e4e685f4bca6ebf2d8dd35f69
2jooho <tjrwn1003@gmail.com>
2022-12-20 23:16:08 +0900

    12/20 aws test

--- src/main/java/com/luck/luckApi/LuckApiApplication.java
+++ src/main/java/com/luck/luckApi/LuckApiApplication.java
@@ -2,12 +2,18 @@ package com.luck.luckApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class LuckApiApplication {
 
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+			+ "classpath:application.yml,"
+			+ "./app/config/newsum/real-application.yml";
 	public static void main(String[] args) {
-		SpringApplication.run(LuckApiApplication.class, args);
+		new SpringApplicationBuilder(LuckApiApplication.class)
+				.properties(APPLICATION_LOCATIONS)
+				.run(args);
 	}
 
 }
--- src/main/java/com/luck/luckApi/controller/LoggingController.java
+++ src/main/java/com/luck/luckApi/controller/LoggingController.java
@@ -1,23 +1,23 @@
-package com.luck.luckApi.controller;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class LoggingController {
-
-    @Value("${logging-module.version}")
-    private String version;
-
-    @GetMapping("/")
-    public String version() {
-        return String.format("Project Version(test) : %s", version);
-    }
-
-    @GetMapping("/health")
-    public String checkHealth() {
-        return "healthy";
-    }
-
-}
+//package com.luck.luckApi.controller;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class LoggingController {
+//
+//    @Value("${logging-module.version}")
+//    private String version;
+//
+//    @GetMapping("/")
+//    public String version() {
+//        return String.format("Project Version(test) : %s", version);
+//    }
+//
+//    @GetMapping("/health")
+//    public String checkHealth() {
+//        return "healthy";
+//    }
+//
+//}

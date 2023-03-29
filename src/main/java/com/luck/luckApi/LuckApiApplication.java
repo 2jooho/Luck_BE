7cda9819311f5c73869df2abbaf02c880a71b502
2jooho <tjrwn1003@gmail.com>
2023-03-29 16:02:11 +0900

    buil를 위한 주석처리

--- src/main/java/com/luck/luckApi/LuckApiApplication.java
+++ src/main/java/com/luck/luckApi/LuckApiApplication.java
@@ -1,14 +1,14 @@
-package com.luck.luckApi;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class LuckApiApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(LuckApiApplication.class, args);
-	}
-
-
-}
+//package com.luck.luckApi;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class LuckApiApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(LuckApiApplication.class, args);
+//	}
+//
+//
+//}

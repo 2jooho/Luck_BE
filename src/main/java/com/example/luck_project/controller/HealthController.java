3964adf45ff9d5cf84ed7ae1a14c0badf3f85f46
2week <2week@ezens.net>
2023-05-15 10:20:48 +0900

    posthelthcheck추가

--- src/main/java/com/example/luck_project/controller/HealthController.java
+++ src/main/java/com/example/luck_project/controller/HealthController.java
@@ -1,6 +1,7 @@
 package com.example.luck_project.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,11 @@ public class HealthController {
 
     @GetMapping("/health")
     public String healtCheck() {
+        return "OK!";
+    }
+
+    @PostMapping("/post/health")
+    public String postHealtCheck() {
         return "OK!";
     }
 }
